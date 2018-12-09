bdc752cc3cb221b641f1e8fe9418be245c7b2a9f
Justus Will <justuswill@gmail.com>
2018-12-09 16:35:47 +0100

    dick angepasst
    Dick nutzt nun die Vektorisierung

--- dick.m
+++ dick.m
@@ -1,9 +1,18 @@
-function y=dick(la,q)
-    y = zeros(3,50);
-    for h = 1:50
-        y(:,h) = simu(h,la,q,10);
+function y=dick(la,q,N)
+    y = zeros(3,1);
+    h = linspace(0.01,50,100);
+    for i = 1:100
+        y(:,i) = neutronblock(h(i),la,q,N)./N;
     end
-    plot(1:50,y(1,:))
-    plot(1:50,y(2,:))
-    plot(1:50,y(3,:))
+    title("Ereignisse")
+    xlabel("h in cm")
+    ylabel("p in %")
+    l1 = "linker Austritt";
+    l2 = "Absorption";
+    l3 = "rechter Austritt";
+    hold on
+    a1 = plot(h,y(1,:));
+    a2 = plot(h,y(2,:));
+    a3 = plot(h,y(3,:));
+    legend([a1;a2;a3],l1,l2,l3)
 end
